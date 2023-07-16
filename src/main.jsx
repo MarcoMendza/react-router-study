a87665602a6126c3d745f44d8aff78d6e5e50d32
MarcoMendza <marco.mendza@outook.com>
2023-07-16 13:40:34 -0600

    Change link and form tags

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Root from './routes/root';
+import Root, {loader as rootLoader, action as rootAction} from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
 
@@ -13,12 +13,18 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root/>,
-    errorElement: <ErrorPage/>
+    errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+    {  
+      path: "contacts/:contactId",
+      element: <Contact/>
+    }
+    ]
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact/>
-  }
+  
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
