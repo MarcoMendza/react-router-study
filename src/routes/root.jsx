d37f64a8267832a446db9221bd18225eeafc77fa
MarcoMendza <marco.mendza@outook.com>
2023-07-18 20:59:43 -0600

    Added delete and edit action

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,7 +2,10 @@ import {
     Outlet, 
     Link,
     useLoaderData,
-    Form
+    Form,
+    redirect,
+    NavLink,
+    useNavigation
 } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
@@ -13,11 +16,13 @@ export async function loader() {
 
 export async function action(){
     const contact = await createContact();
-    return { contact };
+    return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
     const { contacts } = useLoaderData();
+    const navigation = useNavigation();
+
     return (
       <>
         <div id="sidebar">
@@ -50,7 +55,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink 
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -59,7 +73,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -70,7 +84,12 @@ export default function Root() {
           )}
           </nav>
         </div>
-        <div id="detail">
+        <div 
+          id="detail"
+          className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+        >
             <Outlet/>
         </div>
       </>
